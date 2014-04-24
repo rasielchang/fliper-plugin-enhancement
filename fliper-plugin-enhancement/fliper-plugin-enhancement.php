79f921ad95e7b4decbf40fdfee2b82786dc1bd87
Rasiel <rasielchang@gmail.com>
2014-04-24 16:47:33 +0800

    Initial commit.

---
+++ fliper-plugin-enhancement/fliper-plugin-enhancement.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Plugin Name: fliper-plugin-enhancement
+ * Plugin URI: http://company.flipermag.com
+ * Description: Wordpress plugin for FLiPER used, it customize other Wordpress plugins to make things easier.
+ * Version: 0.1.0
+ * Author: Rasiel
+ * Author URI: http://company.flipermag.com
+ */
+
+
+class FliperPluginEnhancement {
+
+	function FliperPluginEnhancement() {
+		add_action( 'admin_init', array( $this, 'init' ) );
+	}
+
+	function init() {
+		if ( is_plugin_active( 'seo-ultimate/seo-ultimate.php' ) ) {
+  			$this->seo_ultimate();
+		} 
+	}
+
+	function seo_ultimate() {
+		add_action( 'save_post', array( $this, 'seo_ultimate_auto_fill' ) );	
+	}
+	
+	/**
+ 	 * Set SEO meta when creating or updating POST.
+ 	 */
+	function seo_ultimate_auto_fill( $post_id ) {
+
+		// Check post type, if it's not post then return
+		if ( 'post' != get_post_type( $post_id ) )
+			return false;
+
+		// Get post data
+		$post_title = get_the_title( $post_id ) . ' | ' . get_bloginfo();
+		$post_content = get_post_field('post_content', $post_id );
+		$post_excerpt = wp_trim_words( $post_content, 140, '...' );
+
+		if ( '' == get_post_meta( $post_id, '_su_title', true ) ) {
+			add_post_meta( $post_id, '_su_title', $post_title, true );
+		} else {
+			update_post_meta( $post_id, '_su_title', $post_title );
+		}
+
+		if ( '' == get_post_meta( $post_id, '_su_og_title' ) ) {
+			add_post_meta( $post_id, '_su_og_title', $post_title, true );
+		} else {
+			update_post_meta( $post_id, '_su_og_title', $post_title );
+		}
+
+		if ( '' == get_post_meta( $post_id, '_su_description' ) ) {
+			add_post_meta( $post_id, '_su_description', $post_excerpt, true );	
+		} else {
+			update_post_meta( $post_id, '_su_description', $post_excerpt );
+		}
+
+		if ( '' == get_post_meta( $post_id, '_su_og_description' ) ) {
+			add_post_meta( $post_id, '_su_og_description', $post_excerpt, true );	
+		} else {
+			update_post_meta( $post_id, '_su_og_description', $post_excerpt );	
+		}
+
+    	return true;
+
+	}
+
+}
+
+$fliper_plugin_enhancement = & new FliperPluginEnhancement();
+
+
+?>
---
+++ fliper-plugin-enhancement/readme.txt
@@ -0,0 +1,14 @@
+=== FLiPER Plugin Enhancement ===
+Contributors: FLiPER Creative Inc., Rasiel Chang
+Tags: FLiPER
+Requires at least: 3.8.1
+Tested up to: 3.8.1
+Stable tag: 0.1.0
+
+Wordpress plugin for FLiPER used, it customize other Wordpress plugins to make things easier.
+
+== Changelog ==
+
+= Version 0.1.0 (April 24, 2014) =
+* Initial release
+* SEO Ultilmate Plugin customize, for automaticly filling post seo data.
